cacd358f54945c066a37497970c3bd60956e77cf
Treyson Searle <treyson.searle96@gmail.com>
2022-09-23 19:38:23 -0700

    initial commit

---
+++ Player.cs
@@ -0,0 +1,14 @@
+using System.Runtime.Remoting.Lifetime;
+using Godot;
+
+namespace Player
+{
+  public class Player : KinematicBody2D
+  {
+        public override void _Ready()
+    {
+      base._Ready();
+    }
+  }
+
+}
---
+++ State.cs
@@ -0,0 +1,32 @@
+using System;
+using Godot;
+
+namespace StateMachine
+{
+  public class State : Node
+  {
+    public StateMachine stateMachine
+    { get; set; }
+
+    public void HandleInput(InputEvent _event)
+    {
+      return;
+    }
+    public void Update(float _delta)
+    {
+      return;
+    }
+    public void PhysicsUpdate(float _delta)
+    {
+      return;
+    }
+    public void OnEnter(Godot.Collections.Dictionary<string, object> _msg)
+    {
+      return;
+    }
+    public void OnExit()
+    {
+      return;
+    }
+  }
+}
